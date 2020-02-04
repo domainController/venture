d8c407208239c0c8c5f56707f904acc1e917938d
kevin-garnette <noyau.imperatif@gmail.com>
2020-02-04 19:26:49 +0000

    le wagon javascript tuto YouTube cQZOfeKrWDs

---
+++ categorie.js
@@ -0,0 +1,74 @@
+ 
+
+ let plane = {
+  genre: "flight ticket",
+  estimation: 600, 
+  type: "transportation", 
+  importance: "incompressible",
+  pourcentage: 20
+};
+ 
+ let smartDrug = {
+  genre: "nootropics",
+  estimation  300, 
+  type: "supplements", 
+  importance: "primordial",
+  pourcentage: 20
+};
+
+ let micronutriment = {
+  genre: "vitamins",
+  estimation: 500, 
+  type: "supplements", 
+  importance: "primordial",
+  pourcentage: 20
+};
+
+ let box = {
+  genre: "self-storage",
+  estimation: 600,
+  type: "logement", 
+  importance: "indispensable",
+  pourcentage: 20
+};
+
+ let foodStock = {
+  genre: "pasta",
+  estimation : 600, 
+  type: "alimentation", 
+  importance: "comfort",
+  pourcentage: 20
+};
+
+ let movingAround = {
+  genre: "bus pass",
+  estimation: 700, 
+  type: "transportation", 
+  importance: "comfort",
+  pourcentage: 20
+ };
+
+ let shower = {
+  genre: "gym pass",
+  estimation: 350, 
+  type: "hygiene", 
+  importance: "comfort",
+  pourcentage: 20
+};
+
+  let backupGear = {
+  genre: "gear upgrade",
+  estimation: 500, 
+  type: "gear", 
+  importance: "comfort",
+  pourcentage: 20
+};
+
+  let miscellaneous = {
+  genre: "misc",
+  estimation: 200, 
+  type: "divers", 
+  importance: "extra",
+  pourcentage: 20
+};
+
