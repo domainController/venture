4e3f9860a7d35cd088e5963bffbea14708d2164d
kevin-garnette <noyau.imperatif@gmail.com>
2020-02-11 13:08:23 +0000

    Mix of DOM manipulation/JSON/CSS fundamentals review before Flex

---
+++ dom/app.js
@@ -0,0 +1,11 @@
+const titles = document.getElementsByClassName('title');
+
+console.log(Array.isArray(titles));
+
+// console.log(Array.isArray(Array.from(titles)));
+
+//Array.from(titles).forEach(function(title){
+//  console.log(title);
+// });
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
