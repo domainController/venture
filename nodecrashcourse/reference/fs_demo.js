81b56f8b1b1721ad912dbe4da63cb3576f650b91
kevin-garnette <noyau.imperatif@gmail.com>
2020-03-01 14:30:10 +0000

    node quick overview

--- nodecrashcourse/reference/fs_demo.js
+++ nodecrashcourse/reference/fs_demo.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-// Create folder
+/*// Create folder
 fs.mkdir(path.join(__dirname, '/test'), {}, err => {
 	if (err) throw err;
 	console.log('Folder created....')
-	});
+	});*/
+
+// Create an write a file
+
+fs.writeFile(path.join(__dirname, '/test', 'Supplements.txt'), 
+	'Micronutrients, Proteins and Nootropics', 
+	err => 
+	{
+		if (err) throw err;
+		console.log('File written to....');
+	}
+);
+//
