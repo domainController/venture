134c007815d76eb6be836acd61c245bf529bdacc
kevin-garnette <noyau.imperatif@gmail.com>
2020-04-15 17:15:04 +0100

    local variable scope

---
+++ portee.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>portee</title>
+		<meta charset='utf-8'>
+	</head>
+	<body>
+		<h1> Notions avancees sur les fonctions</h1>
+		<script>
+			function locale(){
+				var x = 5;
+				alert(x);
+			}
+
+			locale();
+		</script>
+	</body>
+</html>
+
+
+
+<!-- COURS COMPLET JAVASCRIPT [28/65] - La portée en JavaScript -  le 15 avril a l'hostel -->
+
