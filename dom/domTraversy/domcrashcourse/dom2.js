50320be757ec924158484d7c070befda68949542
kevin-garnette <noyau.imperatif@gmail.com>
2020-05-04 19:24:00 +0100

    JavaScript DOM Crash Course - Part 2 and 3

--- dom/domTraversy/domcrashcourse/dom2.js
+++ dom/domTraversy/domcrashcourse/dom2.js
@@ -4,7 +4,7 @@
 // console.log(document.domain);
 // console.log(document.URL);
 // console.log(document.title);
-// document.title = ABC;
+// //document.title =  123;
 // console.log(document.doctype);
 // console.log(document.head);
 // console.log(document.body);
@@ -19,8 +19,8 @@
 // console.log(document.getElementById('header-title'));
 // var headerTitle = document.getElementById('header-title');
 // var header = document.getElementById('main-header');
-// console.log(headerTitle)
-// headerTitle.textContent = 'Kagoshima';
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
 // headerTitle.innerText = 'Goodbye';
 // console.log(headerTitle.innerText);
 // headerTitle.innerHTML = '<h3>Hello</h3>';
@@ -42,15 +42,12 @@
 // }
 
 // GETELEMENTSBYTAGNAME //
-var items = document.getElementsByClassName('list-group-item');
-console.log(items[1].textContent);
-
 // var li = document.getElementsByTagName('li');
 // console.log(li);
 // console.log(li[1]);
 // li[1].textContent = 'Hello 2';
- items[1].style.fontWeight = 'bold';
-  items[1].style.backgroundColor = 'yellow';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
 
 // // Gives error
 // //items.style.backgroundColor = '#f4f4f4';
@@ -60,8 +57,8 @@ console.log(items[1].textContent);
 // }
 
 // QUERYSELECTOR //
- var header = document.querySelector('#main-header');
- header.style.borderBottom = 'solid 4px #ccc';
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
 
 // var input = document.querySelector('input');
 // input.value = 'Hello World'
@@ -92,8 +89,75 @@ console.log(items[1].textContent);
 //   even[i].style.backgroundColor = '#ccc';
 // }
 
-var nls = document.querySelector('#items');
 
-console.log(nls.childNodes);
-nls.parentNode.style.backgroundColor = '#f4f4f4';
 
+// TRAVERSING THE DOM //
+var itemList = document.querySelector('#items');
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// childNodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// // FirstChild
+//console.log(itemList.firstChild);
+// // firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+
+// lastChild
+console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// nextSibling
+// console.log(itemList.nextSibling);
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// previousElementSibling
+// console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
+
+// createElement
+
+// Create a div
+var newDiv =  document.createElement('div');
+
+// Add class
+newDiv.className= 'hello';
+
+// Add id
+newDiv.id = 'hello1';
+
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create text node
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
